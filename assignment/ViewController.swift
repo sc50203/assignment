6fdc7ddaf64ce022e27f52dd64b4f651ff6801b4
leeal <leeal@leealtekiMacBook-Air.local>
2017-12-12 00:02:53 +0800

    Initial Commit

---
+++ assignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  assignment
+//
+//  Created by leeal on 2017/12/12.
+//  Copyright © 2017年 Aptan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
