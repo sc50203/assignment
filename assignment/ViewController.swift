4a025e7c21d334a1d516fd3ff7e36bba0a767269
leeal <leeal@leealtekiMacBook-Air.local>
2017-12-12 00:25:45 +0800

    commit (assignment)

--- assignment/ViewController.swift
+++ assignment/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var randomBall : Int = 0
+    let imageArray = ["ball1","ball2","ball3","ball4","ball5"]
+    
+    @IBOutlet weak var ballImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        UpdateBallImage()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func ask(_ sender: UIButton) {
+        UpdateBallImage()
+    }
+    
+    func UpdateBallImage(){
+        randomBall = Int(arc4random_uniform(5))
+        ballImage.image = UIImage(named:imageArray[randomBall])
+    }
+    
 }
 
